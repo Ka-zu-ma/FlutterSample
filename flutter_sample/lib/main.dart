730e5751ce755f4d3f85519537b2b3a934928fea
Ka-zu-ma <km.km.kzkz00@gmail.com>
2022-01-12 16:40:48 +0900

    基本的なWidgetを実装

--- flutter_sample/lib/main.dart
+++ flutter_sample/lib/main.dart
@@ -49,22 +49,24 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Futterのサンプル!!"),
+        actions: <Widget>[
+          Icon(Icons.add),
+          Icon(Icons.share),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Container(
+        height: double.infinity,
+        color: Colors.red,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            children: <Widget>[
+              Text('Futter'),
+              Text('Futter'),
+              Text('Futter'),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
