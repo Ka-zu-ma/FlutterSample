1181b894cb3c1a5e93c64b3be100fc676edefd08
Ka-zu-ma <km.km.kzkz00@gmail.com>
2022-01-17 18:25:23 +0900

    AppBarのタイトル修正

--- flutter_sample/lib/main.dart
+++ flutter_sample/lib/main.dart
@@ -50,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text("Futterのサンプル!!")
+        title: Text("Flutterのサンプル!!")
       ),
       body: Center(
         child: ElevatedButton(
